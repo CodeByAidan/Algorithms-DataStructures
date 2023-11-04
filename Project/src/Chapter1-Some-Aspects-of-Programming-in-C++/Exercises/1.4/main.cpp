c648cb3c2d9472f7c735b552d48c52d73a43d9ea
CodeByAidan <67598470+CodeByAidan@users.noreply.github.com>
2023-11-04 13:52:42 -0400

    Refactor FromRoman class to remove unused attribute and update constructor signature.

--- Project/src/Chapter1-Some-Aspects-of-Programming-in-C++/Exercises/1.4/main.cpp
+++ Project/src/Chapter1-Some-Aspects-of-Programming-in-C++/Exercises/1.4/main.cpp
@@ -3,25 +3,26 @@
 
 using namespace std;
 
-//int main()
-//{
-//	int a, b;
-//	cout << "Enter two numbers: ";
-//	cin >> a >> b;
-//	try
-//	{
-//		if (b == 0) {
-//			throw runtime_error("Division by zero");
-//			cin.get();
-//			return 1;
-//		};
-//		cout << a << " / " << b << " = " << a / b << endl;
-//	}
-//	catch (runtime_error err)
-//	{
-//		cout << err.what() << endl;
-//		cin.get();
-//		return 1;
-//	}
-//	return 0;
-//}
+int main()
+{
+	int a;
+	int b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	try
+	{
+		if (b == 0) {
+			throw runtime_error("Division by zero");
+			cin.get();
+			return 1;
+		};
+		cout << a << " / " << b << " = " << a / b << endl;
+	}
+	catch (runtime_error& err)
+	{
+		cout << err.what() << endl;
+		cin.get();
+		return 1;
+	}
+	return 0;
+}
