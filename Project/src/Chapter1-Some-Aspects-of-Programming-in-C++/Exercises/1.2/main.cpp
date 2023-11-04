4cc4958605b42dd6b275faf07e76f14b620a5e19
CodeByAidan <67598470+CodeByAidan@users.noreply.github.com>
2023-11-04 13:52:25 -0400

    Refactor main.cpp and ReversePrint.h files, fixing code indentation and namespace usage.

--- Project/src/Chapter1-Some-Aspects-of-Programming-in-C++/Exercises/1.2/main.cpp
+++ Project/src/Chapter1-Some-Aspects-of-Programming-in-C++/Exercises/1.2/main.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-//int main() {
-//    cout << "Enter a sequence of positive integers followed by 0:\n";
-//    reversePrint(cin);
-//    cout << endl;
-//    return 0;
-//}
+int main() {
+    cout << "Enter a sequence of positive integers followed by 0:\n";
+    reversePrint(cin);
+    cout << endl;
+    return 0;
+}
