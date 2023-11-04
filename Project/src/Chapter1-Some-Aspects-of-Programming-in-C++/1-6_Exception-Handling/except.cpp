fe201f89a7362fe3a84b4616cb62502c2a99881d
CodeByAidan <67598470+CodeByAidan@users.noreply.github.com>
2023-11-04 13:50:56 -0400

    Update except.cpp to remove commented out code and reformat main function.

--- Project/src/Chapter1-Some-Aspects-of-Programming-in-C++/1-6_Exception-Handling/except.cpp
+++ Project/src/Chapter1-Some-Aspects-of-Programming-in-C++/1-6_Exception-Handling/except.cpp
@@ -14,21 +14,22 @@ float ReadIntArray(int *p, int n)
 	return sum / n;
 }
 
-//int main()
-//{	float avg;
-//	int a[10], b[12];
-//	cout << "See what happens if, somewhere in the input,\n"
-//			"you type some incorrect character.\n";
-//	try
-//	{	avg = ReadIntArray(a, 10);
-//		cout << "10 integers read. Average: " << avg << endl;
-//		avg = ReadIntArray(b, 12);
-//		cout << "12 integers read. Average: " << avg << endl;
-//	}
-//	catch (int k)
-//	{	cout << "Only " << k << " integers read "
-//			"due to incorrect input.\n";
-//	}
-//	cout << "End of program.\n";
-//	return 0;
-//}
+int main()
+{	float avg;
+	int a[10];
+	int b[12];
+	cout << "See what happens if, somewhere in the input,\n"
+			"you type some incorrect character.\n";
+	try
+	{	avg = ReadIntArray(a, 10);
+		cout << "10 integers read. Average: " << avg << endl;
+		avg = ReadIntArray(b, 12);
+		cout << "12 integers read. Average: " << avg << endl;
+	}
+	catch (int k)
+	{	cout << "Only " << k << " integers read "
+			"due to incorrect input.\n";
+	}
+	cout << "End of program.\n";
+	return 0;
+}
